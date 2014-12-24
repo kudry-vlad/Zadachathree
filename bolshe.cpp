6e9975e0cffd24e6bef5aab24da09eb3cf989b3c
kudry-vlad <kudry-vlad523@mail.ru>
2014-12-24 06:22:58 +0300

    Update bolshe.cpp

--- bolshe.cpp
+++ bolshe.cpp
@@ -1,116 +1,98 @@
 #include "stdafx.h"
 
 #define N	5
+#define M	6
 
 int gen_random(int rangemin, int rangemax)
 {
 	return rangemin + rand() % (rangemax - rangemin + 1);
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-	bool square[N][N];
+	//	заполняем поле
+	bool square[N][M];
 	srand(time(NULL));
 	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < M; j++)
 		{
 			square[i][j] = (bool)gen_random(0, 1);
 		}
 	}
+	
+	char s[15];
+	std::string r_col[N];
+	std::string r_row[M];
 
+	// считем по столбцам
 	int counter = 0;
 	bool flag = false;
-	std::cout << "\t";
-	// считем по столбцам
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < M; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (square[j][i])
-			{
-				counter++;
-			}
+			if (square[j][i])	counter++;
 			else if (counter)
 			{
-				if (flag)
-				{
-					std::cout << ", " << counter;
-				}
-				else
-				{
-					std::cout << counter;
-				}
+				if (flag)	r_row[i] += ",";
+				sprintf_s(s, "%d", counter);
+				r_row[i] += s;
 				flag = true;
 				counter = 0;
 			}
 		}
 		if (counter)
 		{
-			if (flag)
-			{
-				std::cout << ", " << counter;
-			}
-			else
-			{
-				std::cout << counter;
-			}
+			if (flag)	r_row[i] += ",";
+			sprintf_s(s, "%d", counter);
+			r_row[i] += s;
 			counter = 0;
 		}
 		else if (flag == false)
-		{
-			std::cout << "0";
-		}
+			r_row[i] += "0";
 		flag = false;
-		std::cout << "\t";
 	}
-	std::cout << std::endl;
 	//	считаем по строкам
 	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < M; j++)
 		{
-			if (square[i][j])
-			{
-				counter++;
-			}
+			if (square[i][j])	counter++;
 			else if (counter)
 			{
-				if (flag)
-				{
-					std::cout << ", " << counter;
-				}
-				else
-				{
-					std::cout << counter;
-				}
+				if (flag)	r_col[i] += ",";
+				sprintf_s(s, "%d", counter);
+				r_col[i] += s;
 				flag = true;
 				counter = 0;
 			}
 		}
 		if (counter)
 		{
-			if (flag)
-			{
-				std::cout << ", " << counter;
-			}
-			else
-			{
-				std::cout << counter;
-			}
+			if (flag)	r_col[i] += ",";
+			sprintf_s(s, "%d", counter);
+			r_col[i] += s;
 			counter = 0;
 		}
 		else if (flag == false)
-		{
-			std::cout << "0";
-		}
+			r_col[i] += "0";
 		flag = false;
-		std::cout << "\t";
-		//	попутно выводим значения массива
-		for (int j = 0; j < N; j++)
-		{
+	}
+
+
+	//	Выведем реузльтат
+		//	Результат по столбцам
+	std::cout << "\t";
+	for (int i = 0; i < M; i++)
+		std::cout << r_row[i] << "\t";
+	std::cout << std::endl;
+		//	Результат по строкам
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << r_col[i] << "\t";
+		for (int j = 0; j < M; j++)
 			std::cout << square[i][j] << "\t";
-		}
 		std::cout << std::endl;
 	}
 	system("pause");
